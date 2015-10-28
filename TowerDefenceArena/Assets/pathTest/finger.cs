5f0b279ff27a3098f8d2c6b6a7456ea82f4b1469
unknown <mark6717@VIDF0DEF1FC77DE.djes.dk>
2015-10-28 14:06:12 +0100

    pathFinding testing

---
+++ TowerDefenceArena/Assets/pathTest/finger.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class finger : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    RaycastHit hit;
+    public GameObject obj;
+    // Update is called once per frame
+    void Update()
+    {
+        #region touch
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            Ray rayt = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            if (Physics.Raycast(rayt, out hit))
+            {
+                if (hit.collider.tag == "floor")
+                {
+                    Instantiate(obj, hit.point, transform.rotation);
+                }
+            }
+
+        }
+        #endregion
+        #region mouse
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Ray raym = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(raym, out hit))
+            {
+                if (hit.collider.tag == "floor")
+                {
+                    Instantiate(obj, hit.point, transform.rotation);
+                }
+            }
+        }
+        #endregion
+    }
+}
--- TowerDefenceArena/Assets/pathTest/test script.cs
+++ TowerDefenceArena/Assets/pathTest/test script.cs
@@ -4,8 +4,10 @@ using System.Collections;
 public class testscript : MonoBehaviour {
 
 	// Use this for initialization
+    NavMeshObstacle obs;
 	void Start () {
-	
+        obs = GetComponent<NavMeshObstacle>();
+        obs.carving = true;
 	}
 	
 	// Update is called once per frame
