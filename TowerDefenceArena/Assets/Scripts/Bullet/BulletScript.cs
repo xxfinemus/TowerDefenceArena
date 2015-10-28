000e4b9a4a6828e667145bb52ff7666820f2d138
Josefine Nielsen <xpj_21@hotmail.com>
2015-10-28 13:34:12 +0100

    ObjectPool- og BulletScripts

---
+++ TowerDefenceArena/Assets/Scripts/Bullet/BulletScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletScript : MonoBehaviour
+{
+    //The speed of the bullet;
+    public float speed = 5;
+    
+    /// <summary>
+    /// Moves the bullet.
+    /// </summary>
+    void Update()
+    {
+        transform.Translate(0, speed * Time.deltaTime, 0);
+    }
+}
