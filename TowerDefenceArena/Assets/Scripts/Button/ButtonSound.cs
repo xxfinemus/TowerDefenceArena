79ad4625360daf792e4704db2a3a4e1eb3535995
Mikkel Sommer Kristensen <mikkelsk@sol.dk>
2015-10-29 12:13:55 +0100

    Updated: "Mute"-button. Added click sounds to buttons in the main menu.

---
+++ TowerDefenceArena/Assets/Scripts/Button/ButtonSound.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonSound : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource audioSource;
+    [SerializeField]
+    private AudioClip soundClick;
+    [SerializeField]
+    private AudioClip soundRelease;
+
+    public void PlaySoundPress()
+    {
+        audioSource.PlayOneShot(soundClick);
+    }
+
+    public void PlaySoundReleased()
+    {
+        audioSource.PlayOneShot(soundRelease);
+    }
+}
