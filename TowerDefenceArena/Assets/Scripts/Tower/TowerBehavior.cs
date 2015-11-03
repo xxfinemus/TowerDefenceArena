9b6bcd335e5352be27d0580733d7a3d8040c30a0
BenjaminBibby <benjaminbibby@hotmail.com>
2015-11-03 14:10:53 +0100

    TowerBehvior - Done, WallBehavior - Started

--- TowerDefenceArena/Assets/Scripts/Tower/TowerBehavior.cs
+++ TowerDefenceArena/Assets/Scripts/Tower/TowerBehavior.cs
@@ -6,16 +6,24 @@ public class TowerBehavior : MonoBehaviour {
 
     [SerializeField]
     private string name;
+
     [SerializeField]
     private BulletObjectPoolScript objectPool;
+
     [SerializeField]
     private float range;
+
     [SerializeField]
     private float rateOfFire;
+
     [SerializeField]
-    private Queue<GameObject> enemies;
+    private Queue<GameObject> enemies = new Queue<GameObject>();
+
+    public GameObject[] arrayEnemies;
+
     [SerializeField]
     private GameObject target;
+
     public string Name
     {
         get { return name; }
@@ -29,8 +37,25 @@ public class TowerBehavior : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        transform.LookAt(target.transform);
+        LockToTarget();
+        AddToQueue();
+        ClearQueue();
+        target = SetTarget();
+        if (target != null)
+        {
+            Attack(target);
+        }
+
 	}
+    private void LockToTarget()
+    {
+        if (target != null)
+        {
+            Vector3 targetPos = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+            transform.LookAt(targetPos);
+        }
+    }
+    // Add enemy to queue
     private void AddToQueue()
     {
         GameObject[] _enemies = GameObject.FindGameObjectsWithTag("Enemy");
@@ -41,10 +66,12 @@ public class TowerBehavior : MonoBehaviour {
                 if (Vector3.Distance(transform.position, enemy.transform.position) < range)
                 {
                     enemies.Enqueue(enemy);
+                    arrayEnemies = enemies.ToArray();
                 }
             }
         }
     }
+    // If the front enemy is out of range, then put in the back of the queue
     private void DequeueEnemy()
     { 
         Transform _enemy = enemies.Peek().transform;
@@ -53,25 +80,36 @@ public class TowerBehavior : MonoBehaviour {
             enemies.Dequeue();
         }
     }
+    // Set target equal to front element of queue
     private GameObject SetTarget()
     {
         if(enemies.Count > 0)
+        {
+            DequeueEnemy();
+            return enemies.Peek();
+        }
+        return null;
+    }
+    // Clear queue
+    private void ClearQueue()
+    {
+        if (enemies.Count > 0)
         {
             foreach (GameObject obj in enemies)
             {
                 if (Vector3.Distance(transform.position, obj.transform.position) < range)
                 {
-                    return obj;
+                    return;
                 }
             }
+            enemies.Clear();
         }
-        return null;
     }
     private void Attack(GameObject enemy)
     {
 
-        GameObject bullet = objectPool.GetPooledObject();
-        // bullet.transform.position = transform.position;
+        //GameObject bullet = objectPool.GetPooledObject();
+        //bullet.transform.position = transform.position;
         // bullet.GetComponent<FireScript>().Target(enemy);
         // bullet.GetComponent<FireScript>().Fire();
     }
