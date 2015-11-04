782b66463f36280a8ea97ccc50ca7e23e9436725
Valdemar Voss <Vixitdk@gmail.com>
2015-11-04 10:33:02 +0100

    optimized bullet behaviro

--- TowerDefenceArena/Assets/Scripts/BulletObjectpool/SecondBulletScript.cs
+++ TowerDefenceArena/Assets/Scripts/BulletObjectpool/SecondBulletScript.cs
@@ -4,9 +4,25 @@ using System.Collections;
 public class SecondBulletScript : MonoBehaviour
 {
     //The speed of the bullet;
+    Vector2 start;
+    Vector2 end;
+    Vector2 mid;
+    float A ;
+    float B;
+    float D;
+    float AA;
+    float BB;
+    float DD;
+    float BM;
+    float AAA;
+    float DDD;
+    float a;
+    float b;
+    float c;
+    Vector3 oldTargetPos;
     public float speed;
     [SerializeField]
-    private GameObject target;
+    private Transform target;
     [SerializeField]
     private float gravity;
     private float distance;
@@ -18,67 +34,65 @@ public class SecondBulletScript : MonoBehaviour
         get { return startPosition; }
         set { startPosition = value; }
     }
-    private float flightTime;
     private float angle;
     private float distanceTraveled;
 
-    [SerializeField]
-    private float testTravel;
 
-    public GameObject Target
+    public Transform Target
     {
         set { target = value; }
     }
 
     void OnEnable()
     {
+        oldTargetPos = new Vector3(100, 100, 100);
         startPosition = transform.position;
-        flightTime = 0;
+        hight = target.position.y - startPosition.y;
+        distanceTraveled = 0;
+
         //Debug.Log(startPosition);
 
     }
     void TargetDistance()
     {
-        Vector3 targetPos = target.transform.position;
+        Vector3 targetPos = target.position;
         targetPos.y = startPosition.y;
         distance = Vector3.Distance(startPosition, targetPos);
-        targetPos = startPosition;
-        targetPos.y = transform.position.y;
-        distanceTraveled = Vector3.Distance(targetPos, transform.position);
-        Debug.Log("Distance " + distance);
-        Debug.Log("DistancTraveled " + distanceTraveled);
-        //Debug.Log(startPosition);
-        //Debug.Log(target.position);
-        //Debug.Log(transform.position);
-        hight = target.transform.position.y - startPosition.y;
-        Debug.Log("Height " + hight);
+        //targetPos = startPosition;
+        //targetPos.y = transform.position.y;
+        //distanceTraveled = Vector3.Distance(targetPos, transform.position);
+     
 
     }
     // Use this for initialization
     void Start()
     {
-        
 
+        oldTargetPos = new Vector3(100, 100, 100);
         startPosition = transform.position;
-        flightTime = 0;
+        hight = target.position.y - startPosition.y;
+        distanceTraveled = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        TurnTowardsTarget();
         TargetDistance();
         if (distance <= distanceTraveled)
         {
-            transform.position = target.transform.position;
+            transform.position = target.position;
         }
         else
         {
+            TurnTowardsTarget();            
             transform.Translate(0, 0, speed * Time.deltaTime);
+            distanceTraveled += speed * Time.deltaTime;
+            BetterPath();
         }
         //FireAngle();
         //BulletHeight();
-        BetterPath();
+   
     }
 
     void BulletHeight()
@@ -94,29 +108,36 @@ public class SecondBulletScript : MonoBehaviour
 
     void TurnTowardsTarget()
     {
-        Vector3 targetPos = target.transform.position;
+        Vector3 targetPos = target.position;
         targetPos.y = transform.position.y;
         transform.LookAt(targetPos);
     }
 
     void BetterPath()
     {
-        Vector2 start = new Vector2(0, startPosition.y);
-        Vector2 end = new Vector2(distance, target.transform.position.y);
-        Vector2 mid = new Vector2(distance/2, startPosition.y + 5);
-        float A = -Mathf.Pow(start.x, 2) + Mathf.Pow(mid.x, 2);
-        float B = -start.x + mid.x;
-        float D = -start.y + mid.y;
-        float AA = -Mathf.Pow(mid.x, 2) + Mathf.Pow(end.x, 2);
-        float BB = -mid.x + end.x;
-        float DD = -mid.y + end.y;
-        float BM = -(BB / B);
-        float AAA = BM * A + AA;
-        float DDD = BM * D + DD;
-        float a = DDD / AAA;
-        float b = (D-A*a)/B;
-        float c = start.y - a * Mathf.Pow(start.x, 2) - b * start.x;
+
+        
+        if (Vector3.Distance(target.position, oldTargetPos) >= 1)
+        {
+            oldTargetPos = target.position;
+            start = new Vector2(0, startPosition.y);
+            end = new Vector2(distance, target.position.y);
+            mid = new Vector2(distance / 2, startPosition.y + 5);
+            A = -Mathf.Pow(start.x, 2) + Mathf.Pow(mid.x, 2);
+            B = -start.x + mid.x;
+            D = -start.y + mid.y;
+            AA = -Mathf.Pow(mid.x, 2) + Mathf.Pow(end.x, 2);
+            BB = -mid.x + end.x;
+            DD = -mid.y + end.y;
+            BM = -(BB / B);
+            AAA = BM * A + AA;
+            DDD = BM * D + DD;
+            a = DDD / AAA;
+            b = (D - A * a) / B;
+            c = start.y - a * Mathf.Pow(start.x, 2) - b * start.x;
+        }
         float tempHeight = (float)(a*Mathf.Pow(distanceTraveled, 2) + b * distanceTraveled + c);
+        Debug.Log("TempHeight" + tempHeight);
         Vector3 heightToBe = new Vector3(transform.position.x, tempHeight, transform.position.z);
         transform.position = heightToBe;
 
