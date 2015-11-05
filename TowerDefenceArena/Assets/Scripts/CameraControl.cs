bc425815bb14b16bb0775922d0cc1450ffe8f162
Valdemar Voss <Vixitdk@gmail.com>
2015-11-05 09:57:54 +0100

    Pause funktion, og base for camara control tilføjet

---
+++ TowerDefenceArena/Assets/Scripts/CameraControl.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour 
+{
+    [SerializeField]
+    private float top, bottom, left, right, front, back;
+    private Vector3 zoom1, zoom2;
+	// Use this for initialization
+	void Start () 
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    private void Zoom()
+    {
+        Touch[] myTouches = Input.touches;
+        if (myTouches.Length == 2)
+        {
+            
+        }
+        else
+        {
+
+        }
+
+    }
+
+    private void Move()
+    {
+
+    }
+
+    private void ConfineToBounds()
+    {
+        Vector3 confineVecter = transform.position;
+        if (transform.position.y < bottom)
+        {
+            confineVecter.y = bottom;
+        }
+        else if (transform.position.y > top)
+        {
+            confineVecter.y = top;
+        }
+        if (transform.position.x < back)
+        {
+            confineVecter.x = back;
+        }
+        else if (transform.position.x > front)
+        {
+            confineVecter.x = front;
+        }
+        if (transform.position.z < left)
+        {
+            confineVecter.z = left;
+        }
+        else if (transform.position.z > right)
+        {
+            confineVecter.z = right;
+        }
+    }
+}
