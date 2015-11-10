ba73477788189bd6f7736af68b663b12dd1e1672
Mikkel Sommer Kristensen <mikkelsk@sol.dk>
2015-11-10 17:47:02 +0100

    Clean Up: Buttons, Ingame_TowerDefence & Catapult prefab

---
+++ TowerDefenceArena/Assets/Scripts/Button/ButtonSounds.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonSounds : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    [SerializeField]
+    private AudioClip soundClick;
+    [SerializeField]
+    private AudioClip soundRelease;
+
+    private void Awake()
+    {
+        audioSource = Camera.main.GetComponent<AudioSource>();
+    }
+
+    public void PlaySoundPress()
+    {
+        audioSource.PlayOneShot(soundClick);
+    }
+
+    public void PlaySoundReleased()
+    {
+        audioSource.PlayOneShot(soundRelease);
+    }
+}
