3bf8c671d0da774b4622e087c40d210a951fb63e
Mikkel Sommer Kristensen <mikkelsk@sol.dk>
2015-11-12 23:08:44 +0100

    Continued boss special attack

--- TowerDefenceArena/Assets/Scripts/BossAttack.cs
+++ TowerDefenceArena/Assets/Scripts/BossAttack.cs
@@ -6,8 +6,17 @@ public class BossAttack : MonoBehaviour
     [SerializeField]
     private GameObject meleeCollider;
 
+    [SerializeField]
+    private float specialAttackSpeed = 100;
+
+    [SerializeField]
+    private GameObject fireSystem;
+
     private Animator bossAnimator;
+
     private bool specialAttackInProgress = false;
+    private float specialAttack1Range = 0;
+    private float specialAttack1MaxRange = 50;
 
     // Use this for initialization
     void Start()
@@ -18,17 +27,27 @@ public class BossAttack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            specialAttackInProgress = true;
+        }
     }
 
     void FixedUpdate()
     {
-
-        LogicSpecialAttack1();
-
         if (specialAttackInProgress)
         {
+            if (specialAttack1Range <= specialAttack1MaxRange)
+            {
+                specialAttack1Range += 1 * (specialAttackSpeed / 1000);
 
+                LogicSpecialAttack1(specialAttack1Range);
+            }
+            else
+            {
+                specialAttack1Range = 0;
+                specialAttackInProgress = false;
+            }  
         }
 
     }
@@ -52,32 +71,32 @@ public class BossAttack : MonoBehaviour
     /// This methode is heaviely inspiered by "alucardj's" answer to the folowing post:
     /// http://answers.unity3d.com/questions/362763/raycast-over-arc.html
     /// </summary>
-    public void LogicSpecialAttack1()
+    public void LogicSpecialAttack1(float distance)
     {
-        float distance = 5;
         int angle = 360;
 
         int segments = 45;
         int angleStep = angle / segments;
-        Vector3 defaultPos = transform.position + new Vector3(0, 0.2f, 0);
-        Vector3 tagetPos = Vector3.zero;
+        Vector3 defaultPos = transform.position  + new Vector3(0, 0.2f, 0);
+        Vector3 tagetPos = defaultPos;
 
         RaycastHit hit;
 
         for (int i = 0; i <= 360; i += 2)
         {
-            tagetPos = (Quaternion.Euler(0, i, 0) * transform.up).normalized * distance;
-            tagetPos += new Vector3(0, 0.2f, 0);
+            tagetPos = ((Quaternion.Euler(0, i, 0) * transform.forward).normalized * distance) + defaultPos;
 
-            Debug.DrawLine(defaultPos, tagetPos, Color.green);
-        }
+            if (Physics.Raycast(defaultPos, tagetPos, out hit, distance))
+            {
+                if (hit.collider.gameObject.tag == "Player")
+                {
+                    // Player was hit
+                    Debug.Log("The player was hit");
+                }
+            }
 
-        if (Physics.Linecast(defaultPos, tagetPos, out hit))
-        {
-
-        }
-
-           
+            Debug.DrawRay(defaultPos, tagetPos, Color.green);
+        }           
     }
 
     /// <summary>
