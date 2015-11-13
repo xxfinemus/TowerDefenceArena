0b27d7a1e3546a5d7ebdafa5b39d8bfc76fd9840
Mikkel Sommer Kristensen <mikkelsk@sol.dk>
2015-11-13 12:50:20 +0100

    Enemy animations

---
+++ TowerDefenceArena/Assets/Scripts/Enemy/OnDeath.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnDeath : MonoBehaviour
+{
+    [SerializeField]
+    private int timer;
+    
+    private bool die = false;
+    private int counter;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (die)
+        {
+            if (counter <= timer)
+            {
+                transform.position += new Vector3(0, -0.05f, 0);
+                counter++;
+            }
+            else
+            {
+                transform.parent.GetComponent<EnemyHealthScript>().OnDeath();
+
+                die = false;
+                counter = 0;
+            }
+        }
+    }
+
+    public void Die()
+    {
+        die = true;
+    }
+}
