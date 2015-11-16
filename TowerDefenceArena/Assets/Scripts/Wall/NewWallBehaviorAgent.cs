d0e94811f02f3c4068240d8c8e5becb02b6e3343
BenjaminBibby <benjaminbibby@hotmail.com>
2015-11-16 15:46:23 +0100

    NewWallBehavior and stuff

---
+++ TowerDefenceArena/Assets/Scripts/Wall/NewWallBehaviorAgent.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewWallBehaviorAgent : MonoBehaviour
+{
+    public static NewWallBehaviorAgent WallAgent;
+
+    [SerializeField]
+    private GameObject wallModel;
+
+    [SerializeField]
+    private GameObject[] walls;
+
+    [SerializeField]
+    private Transform[][] corners;
+
+    private Vector3 sizeOfWall = Vector3.zero;
+
+    private int wallCount = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        WallAgent = this;
+
+        sizeOfWall = wallModel.transform.GetChild(0).GetComponent<Renderer>().bounds.extents;
+
+        walls = GameObject.FindGameObjectsWithTag("Wall");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameObject.FindGameObjectsWithTag("Wall").Length > walls.Length)
+        {
+            walls = GameObject.FindGameObjectsWithTag("Wall");
+            corners = GetAllCorners();
+            RemoveCorners();
+        }
+        else if(GameObject.FindGameObjectsWithTag("Wall").Length < walls.Length)
+        {
+            walls = GameObject.FindGameObjectsWithTag("Wall");
+            AddCorners();
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            GameObject newWall = InstantiateWall(new Vector3(0 + (sizeOfWall.x * 2 * wallCount), 0, 0));
+            wallCount++;
+        }
+    }
+
+    public GameObject InstantiateWall(Vector3 pos)
+    {
+        GameObject _wall = Instantiate(wallModel, pos, Quaternion.identity) as GameObject;
+        _wall.transform.localEulerAngles = Vector3.zero;
+        return _wall;
+    }
+
+    private Transform[][] GetAllCorners()
+    {
+        Transform[][] _corners = new Transform[walls.Length][];
+        for (int i = 0; i < walls.Length; i++)
+        {
+            _corners[i] = new Transform[walls[i].transform.childCount];
+            int j = 0;
+            foreach(Transform _corner in walls[i].transform)
+            {
+                _corners[i][j] = _corner;
+                j++;
+            }
+        }
+        return _corners;
+    }
+
+    public void RemoveCorners()
+    {
+        for (int i = 0; i < walls.Length; i++)
+        {
+            foreach (Transform _corner in corners[i])
+            {
+                for (int j = 0; j < walls.Length; j++)
+                {
+                    if(walls[i] != walls[j])
+                    {
+                        foreach (Transform _compare in corners[j])
+                        {
+                            if(_corner.gameObject.activeInHierarchy == true && Vector3.Distance(_corner.position, _compare.position) < 0.2f)
+                            {
+                                _compare.gameObject.SetActive(false);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    public void AddCorners()
+    {
+        for(int i = 0; i < walls.Length; i++)
+        {
+            foreach(Transform _corner in corners[i])
+            {
+                if(_corner.gameObject.activeInHierarchy == false)
+                {
+                    for(int j = 0; j < walls.Length; j++)
+                    {
+                        if(walls[i] != walls[j])
+                        {
+                            foreach(Transform _compare in corners[j])
+                            {
+                                if(!(Vector3.Distance(_corner.position, _compare.position) < 0.2f))
+                                {
+                                    _corner.gameObject.SetActive(true);
+                                    return;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- TowerDefenceArena/Assets/Scripts/Wall/WallBehaviorAgent.cs
+++ TowerDefenceArena/Assets/Scripts/Wall/WallBehaviorAgent.cs
@@ -8,21 +8,12 @@ public class WallBehaviorAgent : MonoBehaviour {
     // Field
     [SerializeField]
     private GameObject wallModel;
-    [SerializeField]
-    private GameObject[] walls;
 
     //private Transform[][] corners;
 	// Use this for initialization
 	void Start ()
     {
         WallAgent = this;
-
-        walls = GameObject.FindGameObjectsWithTag("Wall");
-        for (int i = 0; i < walls.Length; i++)
-        {
-            GameObject[] _neighbours = GetNeighbours(walls[i]);
-            //if(walls[i])
-        }
 	}
 	
 	// Update is called once per frame
@@ -69,19 +60,19 @@ public class WallBehaviorAgent : MonoBehaviour {
     public void InstantiateWall(Vector3 pos)
     {
         GameObject _wall = Instantiate(wallModel, pos, Quaternion.identity) as GameObject;
-        _wall.transform.localEulerAngles = new Vector3(270, 0, 0);
+        _wall.transform.localEulerAngles = new Vector3(0, 0, 0);
 
-        GameObject[] _neighbours = GetNeighbours(_wall);    // Neighbours of the wall
+        Transform[] _neighbours = GetNeighbours(_wall);    // Neighbours of the wall
         Transform[] _corners = GetCornersInWall(_wall);     // Corners of the wall
         // Get all neighbours from wall individualy
-        foreach (GameObject neighbour in _neighbours)
+        foreach (Transform neighbour in _neighbours)
         {
-            Transform[] n_corners = GetCornersInWall(neighbour);
-            foreach (Transform n_corner in n_corners)
+            Transform[] n_corners = GetCornersInWall(neighbour.gameObject);
+            foreach (Transform n_corner in n_corners)   // Neighbor wall's corners
             {
-                foreach (Transform t_corner in _corners)
+                foreach (Transform t_corner in _corners)    // This wall's corners
                 {
-                    if (Vector3.Distance(t_corner.position, n_corner.position) < 5f)
+                    if (Vector3.Distance(t_corner.position, n_corner.position) < 15f)
                     {
                         Destroy(t_corner.gameObject);
                     }
@@ -90,7 +81,7 @@ public class WallBehaviorAgent : MonoBehaviour {
         }
     }
     // Get all corners from all walls
-    private Transform[][] GetAllCorners()
+    private Transform[][] GetAllCorners(GameObject[] walls)
     {
         Transform[][] _corners = new Transform[walls.Length][];
         for (int i = 0; i < _corners.Length; i++)
@@ -110,19 +101,20 @@ public class WallBehaviorAgent : MonoBehaviour {
         return _childs.ToArray();
     }
     // Find neighbours to a speciffic wall
-    private GameObject[] GetNeighbours(GameObject wall)
+    private Transform[] GetNeighbours(GameObject wall)
     {
-        List<GameObject> _neighbours = new List<GameObject>();
+        List<Transform> _neighbours = new List<Transform>();
         Vector3[] dirs = { -Vector3.forward, Vector3.left, Vector3.forward, Vector3.right};
         int count = 0;
         for (int i = 0; i < 4; i++)
-        { 
+        {
+            Debug.DrawRay(wall.transform.position + new Vector3(0, 1, 0), dirs[i] * 5f, Color.red, 5f);
             RaycastHit hit;
-            if(Physics.Raycast(wall.transform.position, dirs[i], out hit, 5f))
+            if(Physics.Raycast(wall.transform.position + new Vector3(0, 1, 0), dirs[i], out hit, 5f))
             {
                 if (hit.collider.tag == "Wall")
                 {
-                    _neighbours.Add(hit.collider.gameObject);
+                    _neighbours.Add(hit.collider.transform);
                     Debug.Log(string.Format("Name: {0}->\t Neighbour[{1}]\t Name:{2}\t Pos:{3}", wall.name, count, _neighbours[count].name, _neighbours[count].transform.position));
                     count++;
                 }
